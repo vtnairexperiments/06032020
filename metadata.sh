71a2474982ac27f70bd298fe70f7b81bc29267e5
vtnairexperiments <vtnair@hotmail.com>
2020-03-08 01:00:54 +0000

    metadata.sh added

--- metadata.sh
+++ metadata.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+url="http://169.254.169.254/latest/meta-data/"
 
+list=`curl $url`
+
+echo $list > file.txt
+
+for i in `cat file.txt`
+do
+echo $i
+new=`curl http://169.254.169.254/latest/meta-data/$i`
+
+echo "$i : $new" >> final.json
+done
