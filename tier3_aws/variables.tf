b0d6b4b1a4616e89ca8ea30de4731c93ee8c759c
vtnairexperiments <vtnair@hotmail.com>
2020-03-08 00:35:56 +0000

    varible assigned

---
+++ tier3_aws/variables.tf
@@ -0,0 +1,12 @@
+variable "ami" {}
+variable "insance_type" {}
+variable "region" {}
+variable "access" {}
+variable "secret" {}
+variable "vpc_cidr" {}
+variable "pri_cidr" {}
+variable "pri_cidr" {}
+variable "pub_cidr" {}
+variable "db_user" {}
+variable "db_password" {}
+
