94e4b155497f1185817ecc6f4d729b42b81d59fd
vtnairexperiments <vtnair@hotmail.com>
2020-03-08 00:12:25 +0000

    syntax error with nested.py fixed

--- nested.py
+++ nested.py
@@ -3,8 +3,8 @@
 def test(object,value):
     for key in object:
         if key == value:
-            print(object[key]
+            print(object[key])
         else:
             test(object[key],value)
 
-test({'a':{'b':{'c':'d'}}},"a")
+test({'a':{'b':{'c':'d'}}},"b")
