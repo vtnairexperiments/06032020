432a08524fc14c838811a2cb0649be6cd40fe805
vtnairexperiments <vtnair@hotmail.com>
2020-03-08 00:08:43 +0000

    nested.py added

---
+++ nested.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+def test(object,value):
+    for key in object:
+        if key == value:
+            print(object[key]
+        else:
+            test(object[key],value)
+
+test({'a':{'b':{'c':'d'}}},"a")
